ffb68211f4f25dd2c2b1be49b8d1dcb550881bfd
Santosh Narayanan <santosh33@gmail.com>
2023-02-27 00:26:11 +0100

    Handling Pending Queries

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -2,11 +2,24 @@ import React,{ Component } from 'react';
 import { gql } from '@apollo/client'; //Fetch graphql query
 import  {graphql} from '@apollo/client/react/hoc'; //bind component and query
 class SongList extends Component{
+    renderSongs(){
+        return this.props.data.songs.map(song=> {
+            return(
+                <li key={song.id}>
+                    {song.title}
+            </li>
+            );
+        });
+    }
     render(){
-        console.log(this.props);
+        //if the query is still fething data then do not load entire component
+        if(this.props.data.loading)
+        {
+            return(<div>Still Loading...</div>);
+        }
         return(
             <div>
-                SongList
+                {this.renderSongs()}
             </div>
         );
     }
