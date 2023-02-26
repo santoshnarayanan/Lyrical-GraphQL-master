aaa3261b83d362113155204b064d05400c10939e
Santosh Narayanan <santosh33@gmail.com>
2023-02-26 03:47:03 +0100

    Create SongList Component

---
+++ client/components/SongList.js
@@ -0,0 +1,14 @@
+const { Component } = require("react");
+
+
+class SongList extends Component{
+    render(){
+        return(
+            <div>
+                SongList
+            </div>
+        );
+    }
+}
+
+export default SongList;
