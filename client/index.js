16f4b568b1663204899937c5545e589ca3b08142
Santosh Narayanan <santosh33@gmail.com>
2023-02-27 01:32:36 +0100

    Update Router Path and Switch

--- client/index.js
+++ client/index.js
@@ -1,26 +1,28 @@
 import React from "react";
-import ReactDOM from 'react-dom';
-import { HashRouter, Route } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client";
 
-import SongList from './components/SongList';
-import SongCreate from './components/SongCreate';
+import SongList from "./components/SongList";
+import SongCreate from "./components/SongCreate";
 
-const client  = new ApolloClient({
-  uri:'http://localhost:4000/graphql',
-  cache: new InMemoryCache()
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
 });
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-        <HashRouter>
-          <Route exact path = "/" component={SongList} />
-          <Route path = "song/new" component={SongCreate}/>
-        </HashRouter>
+      <HashRouter>
+        <Switch>
+          <Route path="/songs" exact component={() => <SongList />} />
+          <Route path="/songs/new" exact component={() => <SongCreate />} />
+        </Switch>
+      </HashRouter>
     </ApolloProvider>
   );
 };
 
-ReactDOM.render (<Root />, document.querySelector("#root"));
+ReactDOM.render(<Root />, document.querySelector("#root"));
