e9027c6a35d9c7ac882adb0298f974dc4b351a15
Santosh Narayanan <santosh33@gmail.com>
2023-02-28 02:09:54 +0100

    Navigation on successful mutation

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,7 +1,7 @@
 import React,{ Component } from 'react';
 import { gql } from '@apollo/client'; //Fetch graphql query
 import  {graphql} from '@apollo/client/react/hoc'; //bind component and query
-
+import { Link, hashHistory } from "react-router-dom";
 class SongCreate extends Component{
     constructor(props){
         super(props);
@@ -11,17 +11,19 @@ class SongCreate extends Component{
 
     onSubmit(event){
         event.preventDefault();
+
         this.props.mutate({
             variables: {
                 title: this.state.title
             }
-        });
-
+        }).then(()=>hashHistory.push('/songs')) //Here newsong is not rendered automatically without manual refresh
+        .catch(err=>console.log(err));
     }
 
     render(){
         return(
             <div>
+                <Link to="/songs">Back</Link>
                 <h3>Create new Song</h3>
                 <form onSubmit={this.onSubmit.bind(this)}>
                     <label>Song Title:</label>
--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,38 +1,41 @@
-import React,{ Component } from 'react';
-import { gql } from '@apollo/client'; //Fetch graphql query
-import  {graphql} from '@apollo/client/react/hoc'; //bind component and query
-class SongList extends Component{
-    renderSongs(){
-        return this.props.data.songs.map(song=> {
-            return(
-                <li key={song.id} className="collection-item">
-                    {song.title}
-            </li>
-            );
-        });
+import React, { Component } from "react";
+import { gql } from "@apollo/client"; //Fetch graphql query
+import { graphql } from "@apollo/client/react/hoc"; //bind component and query
+import { Link } from "react-router-dom";
+class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map((song) => {
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      );
+    });
+  }
+  render() {
+    //if the query is still fething data then do not load entire component
+    if (this.props.data.loading) {
+      return <div>Still Loading...</div>;
     }
-    render(){
-        //if the query is still fething data then do not load entire component
-        if(this.props.data.loading)
-        {
-            return(<div>Still Loading...</div>);
-        }
-        return(
-            <ul className="collection">
-                {this.renderSongs()}
-            </ul>
-        );
-    }
+    return (
+      <div>
+        <ul className="collection">{this.renderSongs()}</ul>
+        <Link to="/songs/new" className="btn-floating btn-large red right">
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
+    );
+  }
 }
 
 //Fetch Id and title , Id is required to pass key in React component
 const query = gql`
-{
-    songs{
-        id
-        title
+  {
+    songs {
+      id
+      title
     }
-}
+  }
 `;
 
 export default graphql(query)(SongList);
